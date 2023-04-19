5917deaa52c3d6fcd21bc7830a12b162b150181f
DaddyLongLegs142 <124935021+DaddyLongLegs142@users.noreply.github.com>
2023-04-19 10:06:15 -0500

    Implemented printJobs()

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -119,7 +119,20 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+        if (someJobs.size() == 0) {
+            System.out.println("No Results");
+        } else {
 
-        System.out.println("printJobs is not implemented yet");
+
+            for (HashMap<String, String> someJob : someJobs) {
+                System.out.println("\n*****");
+                for (Map.Entry<String, String> field : someJob.entrySet()) {
+                    String output = field.getKey() + ": " + field.getValue();
+                    System.out.println(output);
+                }
+                System.out.println("*****");
+            }
+        }
+//            System.out.println("printJobs is not implemented yet");
     }
 }
